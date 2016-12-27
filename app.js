4514b9593d2132ef0851c819a207547a10e9745f
Angus Morton <angus.m.morton@gmail.com>
2016-12-27 20:20:07 +1100

    Change port to be process.env.PORT in production

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const routes = require('./routes');
 //  Connect all our routes to our application
 app.use('/', routes);
 
-// Turn on that server!
-app.listen(3000, () => {
-  console.log('App listening on port 3000');
+
+const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
 });
